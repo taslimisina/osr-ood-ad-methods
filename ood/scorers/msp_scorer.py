1340ad591f1a94cc65a1fff189b6a3d3e9371133
taslimisina <taslimisina@yahoo.com>
2022-06-30 11:55:57 +0430

    Fix MspScorer

--- ood/scorers/msp_scorer.py
+++ ood/scorers/msp_scorer.py
@@ -1,9 +1,11 @@
 from ood.scorers.scorer import Scorer
 import torch.nn.functional as F
+import numpy as np
 
 to_np = lambda x: x.data.cpu().numpy()
 
 class MspScorer(Scorer):
     def get_score(self, model_output):
         score = to_np(F.softmax(model_output, dim=1))
+        score = np.max(score, axis=1)
         return score
