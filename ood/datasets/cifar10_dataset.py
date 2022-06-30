15532f8c18c8c099f61e1ed34514e1c9b7c7688e
taslimisina <taslimisina@yahoo.com>
2022-06-30 10:51:22 +0430

    Add Cifar10 dataset

---
+++ ood/datasets/cifar10_dataset.py
@@ -0,0 +1,14 @@
+from torchvision.datasets import CIFAR10
+
+from ood.datasets.dataset import Dataset
+
+
+class Cifar10Dataset(Dataset):
+    def get_name(self) -> str:
+        return 'Cifar10'
+
+    def get_num_classes(self) -> int:
+        return 10
+
+    def get_testset(self, transform):
+        return CIFAR10('./cifar10', train=False, transform=transform, download=True)
