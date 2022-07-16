29982e5d143b18635bfa449bdad3d70af01c8eba
taslimisina <taslimisina@yahoo.com>
2022-07-16 18:32:05 +0430

    Add MSP-OE OOD method

--- main_ood.py
+++ main_ood.py
@@ -5,10 +5,17 @@ from ood.datasets.cifar100_dataset import Cifar100Dataset
 from ood.datasets.cifar10_dataset import Cifar10Dataset
 from ood.evaluator import Evaluator
 from ood.ood_methods.msp import Msp
-
+from ood.ood_methods.msp_oe import MspOE
 
 benchmarks = []
+
+# Cifar10
 benchmarks.append(Evaluator(Msp(), Cifar10Dataset(), Cifar100Dataset(), [Auroc(), Aupr(), Fpr()]))
+benchmarks.append(Evaluator(MspOE(), Cifar10Dataset(), Cifar100Dataset(), [Auroc(), Aupr(), Fpr()]))
+
+# Cifar100
+benchmarks.append(Evaluator(Msp(), Cifar100Dataset(), Cifar10Dataset(), [Auroc(), Aupr(), Fpr()]))
+benchmarks.append(Evaluator(MspOE(), Cifar100Dataset(), Cifar10Dataset(), [Auroc(), Aupr(), Fpr()]))
 
 def main():
     for i, evaluator in enumerate(benchmarks):
---
+++ ood/ood_methods/msp_oe.py
@@ -0,0 +1,38 @@
+from torchvision.transforms import transforms
+
+from ood.archs.wrn import WideResNet
+from ood.ood_methods.ood_method import OodMethod
+from ood.scorers.msp_scorer import MspScorer
+import torch
+import requests
+
+
+ckpt_urls = {
+    'Cifar10': 'https://github.com/hendrycks/outlier-exposure/raw/master/CIFAR/snapshots'
+               '/oe_scratch/cifar10_calib_wrn_oe_scratch_epoch_99.pt',
+    'Cifar100': 'https://github.com/hendrycks/outlier-exposure/raw/master/CIFAR/snapshots'
+                '/oe_scratch/cifar100_calib_wrn_oe_scratch_epoch_99.pt'
+}
+
+class MspOE(OodMethod):
+
+    def __init__(self):
+        super().__init__(WideResNet(depth=40, num_classes=10, widen_factor=2, dropRate=0.3), MspScorer())
+
+    def get_transform(self):
+        mean = [x / 255 for x in [125.3, 123.0, 113.9]]
+        std = [x / 255 for x in [63.0, 62.1, 66.7]]
+        test_transform = transforms.Compose([transforms.ToTensor(), transforms.Normalize(mean, std)])
+        return test_transform
+
+    def get_trained_arch(self, dataset):
+        if dataset not in ckpt_urls.keys():
+            print(dataset, 'dataset is not available!')
+            return
+        ckpt_url = ckpt_urls[dataset]
+        response = requests.get(ckpt_url)
+        file = 'msp_checkpoint.pt'
+        with open(file, 'wb') as handle:
+            handle.write(response.content)
+        self.arch.load_state_dict(torch.load(file))
+        return self.arch
